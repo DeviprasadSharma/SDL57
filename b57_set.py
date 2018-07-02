09bde7ea6a6096920e5fe483030685f0791d0ab7
DeviprasadSharma <dns18111998@gmail.com>
2018-07-02 11:43:03 +0530

    first commit

---
+++ b57_set.py
@@ -0,0 +1,16 @@
+A=set([1,2,3,4,5])
+B=set([3,4,5,6,7])
+print(3 in A)
+print(len(A))
+print(len(B))
+
+print("OR OPERATION")
+print(A|B)
+print("AND OPERATION")
+print(A&B)
+print("DIFFERENCE OPERATION")
+print(A-B)
+print("SYMMETRIC DIFFERENCE OPERATION")
+print(A^B)
+
+
